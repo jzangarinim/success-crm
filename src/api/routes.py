0e4374e0b6267ebe72c5a51504c5fd3860d61a96
Paola Mangelli <mangelli.paola90@gmail.com>
2023-07-29 15:50:08 -0400

    modificacion al user - post routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Customer, Project
 from api.utils import generate_sitemap, APIException
+from werkzeug.security import generate_password_hash, check_password_hash
 
 api = Blueprint('api', __name__)
 
 # /users endpoints
+
 @api.route('/users', methods=['GET'])
 def get_users():
     users = User()
@@ -15,45 +17,55 @@ def get_users():
     else:
         return jsonify({"message": "no users found"}), 404
 
+
 @api.route('/users/<int:user_id>', methods=['GET'])
-def get_one_user(user_id = None):
+def get_one_user(user_id=None):
     if user_id is not None:
         users = User()
         users = users.query.get(user_id)
         if users is not None:
             return jsonify(users.serialize()), 200
         else:
-            return jsonify({"message":"user not found"}), 404
+            return jsonify({"message": "user not found"}), 404
     else:
-        return jsonify({"message":"bad request"}), 400
+        return jsonify({"message": "bad request"}), 400
+
 
 @api.route('/users/<int:user_id>/projects', methods=['GET'])
-def get_user_projects(user_id = None):
-    projects = Project.query.filter_by(account_manager_id = user_id).all()
+def get_user_projects(user_id=None):
+    projects = Project.query.filter_by(account_manager_id=user_id).all()
     if len(projects) == 0:
-        projects = Project.query.filter_by(assistant_id = user_id).all()
+        projects = Project.query.filter_by(assistant_id=user_id).all()
         if len(projects) == 0:
-            return jsonify({"message":"no projects found"}), 404
+            return jsonify({"message": "no projects found"}), 404
     projects = list(map(lambda item: item.serialize(), projects))
     return jsonify(projects), 200
 
+
 @api.route('/users/<department>', methods=['GET'])
-def get_department(department = None):
+def get_department(department=None):
     if department == "hr" or department == "sales" or department == "finances" or department == "trial" or department == "recruitment":
         users = User()
-        users = users.query.filter_by(department = department).all()
+        users = users.query.filter_by(department=department).all()
         users = list(map(lambda item: item.serialize(), users))
         if len(users) != 0:
             return jsonify(users), 200
         else:
-            return jsonify({"message":"no users found"}), 404
+            return jsonify({"message": "no users found"}), 404
     else:
-        return jsonify({"message":"bad request"}), 400
+        return jsonify({"message": "bad request"}), 400
+
+def set_password(password, salt):
+    return generate_password_hash(f"{password}{salt}")
+
+def check_password(hash_password, password, salt):
+    return check_password_hash(hash_password, f"{password}{salt}")
 
 @api.route('/users', methods=['POST'])
 def add_user():
     if request.method == "POST":
         data = request.json
+
         if data.get("email") is None:
             return jsonify({"message": "Wrong property"}), 400
         if data.get("password") is None:
@@ -68,33 +80,38 @@ def add_user():
             return jsonify({"message": "Wrong property"}), 400
         if data.get("country") is None:
             return jsonify({"message": "Wrong property"}), 400
-
+        
         user = User.query.filter_by(email=data.get("email")).first()
         if user is not None:
             return jsonify({"message": "The user all ready exist"})
 
         if user is None:
+            salt = b64encode(as.urandom(32)).decode('utf-8')
             user = User(email=data["email"], password=data["password"],
                         department=data["department"], name=data["name"],
                         last_name=data["last_name"], city=data["city"],
-                        country=data["country"])
+                        country=data["country"], salt = salt)
             db.session.add(user)
 
             try:
                 db.session.commit()
-                return jsonify(data), 201
+                return jsonify(body), 201
 
             except Exception as error:
                 print(error)
                 return jsonify({"message": error.args}), 500
 
+    
 # /customers endpoints
+
+
 @api.route('/customers', methods=['GET'])
 def get_customers():
     customers = Customer()
     customers = customers.query.all()
     customers = list(map(lambda item: item.serialize(), customers))
     return jsonify(customers)
+
 
 @api.route('/customers/<int:customer_id>', methods=['GET'])
 def get_one_customer(customer_id=None):
@@ -109,6 +126,8 @@ def get_one_customer(customer_id=None):
         return jsonify({"message": "bad request"}), 400
 
 # /projects endpoints
+
+
 @api.route('/projects', methods=['GET'])
 def get_projects():
     projects = Project()
@@ -117,13 +136,15 @@ def get_projects():
     for project in projects:
         aux_projects.append(project.serialize())
     for project in aux_projects:
-        manager = User.query.filter_by(id=project["account_manager_id"]).first()
+        manager = User.query.filter_by(
+            id=project["account_manager_id"]).first()
         assistant = User.query.filter_by(id=project["assistant_id"]).first()
         customer = User.query.filter_by(id=project["customer_id"]).first()
         project["account_manager_id"] = f"{manager.name} {manager.last_name}"
         project["assistant_id"] = f"{assistant.name} {assistant.last_name}"
         project["customer_id"] = f"{customer.name} {customer.last_name}"
     return jsonify(aux_projects), 200
+
 
 @api.route('/projects/<int:project_id>', methods=['GET'])
 def get_one_project(project_id=None):
@@ -135,7 +156,8 @@ def get_one_project(project_id=None):
         else:
             return jsonify({"message": "project not found"}), 404
     else:
-        return jsonify({"message":"bad request"}), 400
+        return jsonify({"message": "bad request"}), 400
+
 
 @api.route('/projects/<int:project_id>', methods=['PUT'])
 def edit_project(project_id=None):
@@ -164,6 +186,7 @@ def edit_project(project_id=None):
         except Exception as error:
             print(error)
             return jsonify({"message": error.args}), 500
+
 
 @api.route('/projects', methods=['POST'])
 def add_project():
