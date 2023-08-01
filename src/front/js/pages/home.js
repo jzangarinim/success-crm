82dc33ee82e22ac43edfa10dede72894e4108d6b
Jose Zangarini <jzangarinim@gmail.com>
2023-07-31 21:23:08 -0400

    added new logo to Homepage

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,21 +1,16 @@
-import React, { useState, useContext } from "react";
-import { Context } from "../store/appContext";
+import React from "react";
 import "../../styles/home.css";
 import { Link } from "react-router-dom";
+import Logo from "../../img/Logo.png";
 
 export const Home = () => {
-  const { store, actions } = useContext(Context);
   return (
     <>
       <div className="container-fluid d-flex justify-content-center">
         <div className="col-12 card mb-3 mt-3 border border-success">
           <div className="row g-0">
             <div className="col-md-8 d-flex align-items-center">
-              <img
-                src="https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/10/19070942/TED-Talk-What-Is-YOUR-Definition-of-Success.jpg"
-                className="img-fluid rounded-start"
-                alt="..."
-              />
+              <img src={Logo} className="img-fluid rounded-start" alt="..." />
             </div>
             <div className="col-md-4 test">
               <div className="card-body d-flex flex-column align-items-center justify-content-center h-100">
